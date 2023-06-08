0e197314131cd6b3631cc700f8297d0159af7918
helloloop <1204386811@qq.com>
2023-06-08 09:37:48 -0600

    123

--- index.js
+++ index.js
@@ -31,29 +31,37 @@ reader.on("data", (packet) => {
   const clazz = REGISTRY[packet.header.msgid];
   if (clazz) {
     const data = packet.protocol.data(packet.payload, clazz);
-    if (data.constructor.name == "SysStatus")
-      console.log("Received packet: Battery", data.batteryRemaining);
-    if (data.constructor.name == "GlobalPositionInt")
-      console.log(
-        "Received packet:",
-        data.lat / 1e7,
-        data.lon / 1e7,
-        data.alt / 1000
-      );
+
+    console.log(data.constructor.name);
+    if (data.constructor.name == "CommandAck")
+      console.log("Received packet: CommandAck", data);
+
+    if (data.constructor.name == "ParamValue")
+      console.log("Received packet: ParamValue", data);
+    if (data.constructor.name == "MissionAck")
+      console.log("Received packet: MissionAck", data);
+    // if (data.constructor.name == "SysStatus")
+    //   console.log("Received packet: Battery", data.batteryRemaining);
+    // if (data.constructor.name == "GlobalPositionInt")
+    //   console.log(
+    //     "Received packet:",
+    //     data.lat / 1e7,
+    //     data.lon / 1e7,
+    //     data.alt / 1000
+    //   );
   }
 });
 
 const command = new common.RequestProtocolVersionCommand();
-command.confirmation = 1;
 
-const message = new common.ParamRequestList();
+const message = new common.MissionClearAll();
 message.targetSystem = 1;
 message.targetComponent = 1;
+message.mission_type = 0;
 
 port.on("open", async () => {
-  // the port is open - we're ready to send data
-  let res1 = await send(port, command, new MavLinkProtocolV2());
-  console.log("send1 successfully", res1);
+  // let res1 = await send(port, command, new MavLinkProtocolV2());
+  // console.log("send1 successfully", res1);
   let res2 = await send(port, message);
   console.log("send2 successfully", res2);
 });
